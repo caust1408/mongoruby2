565f17b6a22e15aabcf051cd926debc1d3d0b9e2
Brian Capouch <brianc@proto.palaver.net>
2015-04-26 15:16:45 -0400

    Show multiple queries

--- query.rb
+++ query.rb
@@ -2,8 +2,10 @@
 require 'rubygems'
 require 'mongo'
 client = Mongo::Client.new('mongodb://127.0.0.1:27017/cmp341')
+#cursor = client[:numbers].find('num' =>  {'$gt' =>  199995} )
 cursor = client[:users].find('age' =>  {'$gt' =>  20} )
 cursor.each do |doc|
 print doc["last_name"],' : ', doc['age']
+#puts doc["num"]
 puts
 end
