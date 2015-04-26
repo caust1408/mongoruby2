d63faa090b2c694fa60190745af7aaa6b02fcf1e
Brian Capouch <brianc@proto.palaver.net>
2015-04-26 15:17:27 -0400

    Add alternative query

--- resultCount.rb
+++ resultCount.rb
@@ -2,4 +2,5 @@
 require 'rubygems'
 require 'mongo'
 client = Mongo::Client.new('mongodb://127.0.0.1:27017/cmp341')
-p client[:users].find.count
+p client[:numbers].find.count
+#p client[:users].find.count
