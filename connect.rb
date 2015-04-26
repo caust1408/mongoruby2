591c90cdefa4d83373d520f27107297f459b5d44
Brian Capouch <brianc@proto.palaver.net>
2015-04-26 05:00:40 -0400

    "Disable" annoying heartbeat messages

--- connect.rb
+++ connect.rb
@@ -1,4 +1,5 @@
-# Insert a single record
+# Base connector for use with irb
+# Note heartbeat value of 10 minutes--remove this option and weep!
 require 'rubygems'
 require 'mongo'
 #client = Mongo::Client.new('mongodb://127.0.0.1:27017/cmp341')
