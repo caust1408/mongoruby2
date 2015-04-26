01d5d2a5dcb2acb5e2b6a967983058236149cb60
Brian Capouch <brianc@proto.palaver.net>
2015-04-26 15:36:00 -0400

    Continue to discover syntax

--- command.rb
+++ command.rb
@@ -4,5 +4,5 @@ require 'mongo'
 client = Mongo::Client.new('mongodb://127.0.0.1:27017/admin')
 cursor = client.command( {'listDatabases' =>  1} )
 cursor.each do |doc|
-puts doc
+puts doc["databases"]
 end
