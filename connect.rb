02d26432df8cc9781f943ec48edd152f1ab61b84
Brian Capouch <brianc@proto.palaver.net>
2015-04-26 15:16:20 -0400

    Attempt to shut off heartbeat

--- connect.rb
+++ connect.rb
@@ -3,5 +3,5 @@
 require 'rubygems'
 require 'mongo'
 #client = Mongo::Client.new('mongodb://127.0.0.1:27017/cmp341')
-Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'cmp341', :heartbeat_frequency => 600)
+client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'cmp341', :heartbeat_frequency => 600)
 
