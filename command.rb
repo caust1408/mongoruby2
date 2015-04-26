a279cd984601b36ec50a55d1fe6df9aeed0eac47
Brian Capouch <brianc@proto.palaver.net>
2015-04-26 15:44:58 -0400

    Closer to figuring out these ruby hashes

--- command.rb
+++ command.rb
@@ -4,5 +4,9 @@ require 'mongo'
 client = Mongo::Client.new('mongodb://127.0.0.1:27017/admin')
 cursor = client.command( {'listDatabases' =>  1} )
 cursor.each do |doc|
-puts doc["databases"]
+  doc.each do |name|
+  puts "Name:", name
+  end
+puts
+#puts doc["databases"]
 end
